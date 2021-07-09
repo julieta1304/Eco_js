898839ca0d8a480b8b6bbb94f5bcfdfa0aafcff1
Julieta de la Fuente <julieta.dlf97@gmail.com>
2021-07-09 20:10:42 +0200

    agregando jQuery

--- js/main.js
+++ js/main.js
@@ -1,22 +1,37 @@
+
+// Bueno intenté descubrir porqué no me toma el produit.id de la linea 260 y creo que es porque apreto el boton de agregar al carrito se pushea como objeto y no me toma el id de adento entonces me lo muestra como null y creo que por eso depues no encuentra el id, nombre, precio y todo. Debe ser un error chiquito pero no me voy cuenta que toquetear
+
+
+
 // Se carga primer en la ventana
 window.onload = () => {
     renderProduits(produits);
-    renderPanier(panier);
-    const btnAjouterAuPanier = document.querySelectorAll('.container__home--button');
-    btnAjouterAuPanier.forEach(btn => btn.addEventListener('click', AjouterAuPanier));
+    // boton agregar al carrito con evento
+    const btnAjouterAuPanier = document.querySelectorAll(
+        '.container__home--button'
+    );
+    btnAjouterAuPanier.forEach(
+        (btn) => btn.addEventListener("click", (e) => ajouterAuPanier(e))
+    );
+    // boton eliminar del carrito con evento
+    const btnSupprimerAuPanier = document.querySelectorAll(
+        '.container__home--button'
+    );
+    btnSupprimerAuPanier.forEach(
+        (btn) => btn.addEventListener("click", (id) => supprimerAuPanier(id))
+    );
 }
 
 // Funciones de LocalStorage
 function getLocalStorage(key) {
     return JSON.parse(localStorage.getItem(key))
 }
-
 function saveInLocalStorage(key, item) {
     let stringifiedItem = JSON.stringify(item)
     localStorage.setItem(key, stringifiedItem)
 }
 
-// Mi base de datos de todos los productos existentes en la boutique
+// Mi base de datos de todos los productos existentes en la boutique Array de productos y cada uno con sus objetos
 const produits = [
     // Productos del Baño
     {
@@ -174,85 +189,122 @@ const produits = [
         categorie: 'Bébés et Femmes'
     }
 ]
-// array donde se guardan los productos que vamos seleccionando
-let panier = [];
-// Arrow function, interaccion con el HTML
-const renderProduits = produits => {
-    const produitsDiv = document.querySelector('div');
-    //Si el parametro está vacio o es undefined
-    if (!produits || produits.length === 0) {
-        produitsDiv.innerHTML = `<p> Panier vide - Aucun produits </p>`
-        return;
-    }
-    produitsDiv.innerHTML = '';
-    let container = '';
-    // En cada producto del parametro productos se le agrega un contenido que se va a repetir en c/u
-    produits.forEach(produit => {
-        container += `
-                    <div class="container__boutique--card img-${produit.id} col-sm-12 col-md-6 col-lg-4">
-                        <div class="card-body">
-                            <h4 class="container__boutique--card-title">
-                                ${produit.nom}
-                                <span>${produit.prix}</span>
-                            </h4>
-                            <button type="button" class="container__home--button" required="required">
-                                <a class="card-btn" id='${produit.id}' href="../pages/panier.html">
-                                    Ajouter au panier
-                                </a>
-                            </button>
-                        </div>
-                    </div>
-        `;
-    });
-    // Ahora se puede observar que quedo cargado el contenido en la variable creada
-    produitsDiv.innerHTML = container;
-}
+console.log(produits);
+// Array del carrito vacio inicial y en el localStorage
+let panier = getLocalStorage("listeProduitsPanier") || [];
+console.log(panier);
 
-// Arrow function para agregar productos al carrito de compras
-const AjouterAuPanier = evenement => {
-    const idDuProduitAchercher = evenement.target.value;
-    const chercherProduitDB = produits.find(
-        element => element.id === idDuProduitAchercher
-    );
-    panier = getLocalStorage('panier') || [];
-    console.log(panier);
-    panier.push(chercherProduitDB);
-    saveInLocalStorage('listeProduitsPanier', panier);
-    console.log(panier);
-    renderPanier(panier);
-}
-// Se muestran los productos en el HTML
-const renderPanier = produits => {
-    const panierDiv = document.querySelector('.panier');
+// Interaccion con el HTML para renderizar productos
+const renderProduits = (produits) => {
+    const produitsDiv = document.getElementById("60");
     //Si el parametro está vacio o es undefined
-    if (!produits || produits.length === 0) {
-        panierDiv.innerHTML = '<p> Panier vide - Aucun produits </p>'
-        return;
+    if (produitsDiv) {
+        produitsDiv.innerHTML = "";
+        let container = "";
+        // En cada producto de productos se le agrega un contenido que se va a repetir en c/u
+        produits.forEach((produit) => {
+            container = `<div class="container__boutique--card img-prod${produit.id} col-sm-12 col-md-6 col-lg-4">
+                            <div class="card-body">
+                                <h4 class="container__boutique--card-title">
+                                    ${produit.nom}
+                                    <span>${produit.prix}€</span>
+                                </h4>
+                                <button type="button" id="btnAjouterAuPanier" class="container__home--button" required="required">
+                                        Ajouter au panier
+                                </button>
+                                <button type="button" id="btnSupprimerAuPanier" class="container__home--button" required="required">
+                                        Supprimer
+                                </button>
+                            </div>
+                        </div> `;
+            produitsDiv.innerHTML += container;
+        });
     }
-    panierDiv.innerHTML = '';
-    let html = '';
-    produits.forEach(produit => {
-        html += `
-        <div class="produitsBoutique">
-            <h2>${produit.nom}</h2>
-            <button class="btn-supprimer" onclick="supprimerAuPanier('${produit.id}')">X</button>
-        </div>
-        `;
-    });
-    panierDiv.innerHTML = html;
-}
-// Eliminar productos del carrito de compras
-const supprimerAuPanier = id => {
-    console.log(panier)
-    const chercherProduitAuPanier = panier.filter(
-        produit => produit.id !== id
+};
+console.log(panier);
+console.log(produits);
+
+
+// Funcion para agregar productos al carrito de compras
+const ajouterAuPanier = (evenement) => {
+    const idDuProduitAchercher = evenement.target.id;
+    const chercherProduitDB = produits.find(
+        (produit) => produit.id == idDuProduitAchercher
     );
-    console.log(
-        'Carrito sin el producto que eliminaste: ',
-        chercherProduitAuPanier
-    );
+    panier.push(chercherProduitDB);
+    saveInLocalStorage("listeProduitsPanier", panier);
+};
+console.log(panier);
+
+// Renderizar los productos cargados al carrito en el HTML
+const renderPanier = (produits) => {
+    const panierDiv = document.getElementById("80");
+    //Si el parametro está vacio o es undefined
+    if (panierDiv) {
+        panierDiv.innerHTML = "";
+        let html = "";
+        if (!produits || produits.length === 0) {
+            // Esta parte no la veo escrita en ningun lado lo ideal seria que aparezca en el carrito cuando no haya nada 
+            panierDiv.innerHTML = `<p> Panier vide - Aucun produit </p>`;
+            return;
+        }
+        // Aca no sé que pasa que me toma como null los valores de mi array productos, nose si es porque dentro de mi array tengo objetos?
+        produits.forEach(produit => {
+            html = `
+            <div class="produitsPanier text-center col-sm-12 col-md-6 col-lg-4">
+                <h3>
+                ${produit.nom}
+                </h3>
+                <p>${produit.prix}€</p>
+                <button type="button" class="container__home--button btn-supprimer" onclick="supprimerAuPanier('${produit.id}')">Supprimer</button>
+            </div>
+            `;
+            panierDiv.innerHTML += html;
+        });
+    }
+};
+console.log(panier);
+
+
+// Funcion para eliminar productos del carrito de compras
+const supprimerAuPanier = (id) => {
+    const chercherProduitAuPanier= panier.filter(produit => produit.id !== id);
     //Se modifica el carrito original con el nuevo array;
     panier = chercherProduitAuPanier;
     localStorage.removeItem(chercherProduitAuPanier);
     renderPanier(panier);
+}
+console.log(panier);
+
+
+// jQuery, se crean 4 botones para poder filtrar los productos por categorias
+
+// Boton del Baño con selector avanzado
+const boutonSalleDeBains = $('form button:first-child');
+console.log(boutonSalleDeBains);
+
+// Boton de la Cocina con selector combinacion
+const boutonCuisine = $('form #cuisine');
+console.log(boutonCuisine);
+
+// Boton de la Casa con selector combinacion
+const boutonMaison = $('form #maison');
+console.log(boutonMaison);
+
+// Boton Bebés y Mujeres con selector avanzado
+const boutonBebesEtFemmes = $('form button:last-child');
+console.log(boutonBebesEtFemmes);
+
+
+// Agregar método on click a los botones utilizando jQuery
+$('#bouton').click(function produitsPourCategorie(value) {
+    console.log(value.target);
+});
+
+// filtrar productos por categoria
+    const produitsPourCategorie= (value) => {
+    const filterProduitsPourCategorie= produits.filter(produit => produit.categorie !== value);
+    produits = filterProduitsPourCategorie;
+    renderProduits(filterProduitsPourCategorie);
+    console.log(filterProduitsPourCategorie);
 }
